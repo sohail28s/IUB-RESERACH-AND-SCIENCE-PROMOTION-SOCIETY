b5855fed49782597e58b1230bfdc6da7c15c8692
SOHAIL ZAFAR <sohailzafar4u@gmail.com>
2024-08-17 16:07:30 +0500

    blog submitted

---
+++ js/blog-submit.js
@@ -0,0 +1,20 @@
+emailjs.init('n0UUYkJ_Cl_V3UpDu'); // Initialize EmailJS with your user ID
+
+document.getElementById('form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    const btn = document.getElementById('button');
+    btn.value = 'Sending...'; // Change button text to indicate sending
+
+    const serviceID = 'service_ysviq7m'; // Your Service ID
+    const templateID = 'template_5bj77zq'; // Your Template ID
+
+    emailjs.sendForm(serviceID, templateID, this)
+        .then(() => {
+            btn.value = 'Send Email'; // Reset button text
+            alert('Sent!'); // Notify user of success
+        }, (err) => {
+            btn.value = 'Send Email'; // Reset button text
+            alert('Failed to send email: ' + JSON.stringify(err)); // Notify user of error
+        });
+});
